e6c8529f3dfda442a1ab7b3be4eaee75fc5a7d36
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-08-20 16:56:53 +0500

    running the first query

--- logs_analysis.py
+++ logs_analysis.py
@@ -2,8 +2,30 @@
 Logs analysis Udacity Project Full Stack Nano-degree
 """
 #!/usr/bin/env python3
-
+import sys
 import psycopg2
 
-print('Hello World!')
+DBNAME="news"
 
+def execute_query(query):
+
+    try:
+        #Connect to the database named news
+        connection = psycopg2.connect(database=DBNAME)
+        #Open cursor for use with query
+        c = connection.cursor()
+        #Execute the query
+        c.execute(query)
+        #Store the results of the query using fetchall
+        results = c.fetchall()
+        #Close database connection
+        connection.close()
+        #Return the result
+        print(results)
+        return results
+    except psycopg2.Error as e:
+        print(e)
+        sys.exit(1)
+
+if __name__ == '__main__':
+    execute_query("Select * from authors;")
