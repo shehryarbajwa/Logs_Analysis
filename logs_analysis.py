4bd18688227929ce1d488c014eb4a10174ff04d5
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-09-02 03:15:25 +0500

    modified code as per PEP8 Guidelines

--- logs_analysis.py
+++ logs_analysis.py
@@ -1,38 +1,40 @@
-"""
-Logs analysis Udacity Project Full Stack Nano-degree
-"""
 #!/usr/bin/env python3
+"""Logs analysis Udacity Project Full Stack Nano-degree"""
 import sys
 import psycopg2
 
-DBNAME="news"
+
+DBNAME = "news"
+
 
 """Connect to the database and execute the query"""
+
+# Connect to the database named news
+# Open cursor for use with query
+# Execute the query
+# Store the results of the query using fetchall
+# Close database connection
+# Return the result
+
 
 def execute_query(query):
 
     try:
-        #Connect to the database named news
         connection = psycopg2.connect(database=DBNAME)
-        #Open cursor for use with query
         c = connection.cursor()
-        #Execute the query
         c.execute(query)
-        #Store the results of the query using fetchall
         results = c.fetchall()
-        #Close database connection
         connection.close()
-        #Return the result
         print(results)
         return results
     except psycopg2.Error as e:
         print(e)
         sys.exit(1)
 
-"""Run each of the three queries"""
 
 question_1 = "What are the most popular three articles of all time?"
-query_1 =  """
+
+query_1 = """
 select title, count(*) as views from articles inner join
 log on concat('/article/', articles.slug) = log.path
 where log.status like '%200%'
@@ -41,7 +43,7 @@ group by articles.title order by views desc limit 3;
 
 question_2 = "What are the most popular article authors of all time?"
 query_2 = """
-select authors.name, count(*) as views from articles 
+select authors.name, count(*) as views from articles
 inner join authors on articles.author = authors.id
 inner join log on concat('/article/', articles.slug) = log.path
 where log.status like '%200%'
@@ -56,7 +58,8 @@ select * from (
     as errorpercentage from
     (select date(time) as day, count(*) as count from log group by day) as a
     inner join
-    (select date(time) as day, count(*) as count from log where log.status like '%404%' group by day) as b
+    (select date(time) as day, count(*) as count from log where
+    log.status like '%404%' group by day) as b
     on a.day = b.day)
     as p where errorpercentage > 1.0;
 """
@@ -70,6 +73,7 @@ print(
     "\n"
 )
 
+
 def print_results(q_list):
     for i in range(len(q_list)):
         title = q_list[i][0]
@@ -77,9 +81,28 @@ def print_results(q_list):
         print("\t" + "%s - %d" % (title, res) + " views")
     print("\n")
 
+
 print(question_1)
+
+
 print_results(q1)
+
+
 print(question_2)
+
+
 print_results(q2)
+
+
 print(question_3)
-print("\t" + str(q3[0][0]) + "  " + str(q3[0][1]) + " %") 
+
+
+def print_q3(query):
+    for key, value in query:
+        print("\t{} - {} %".format(key, value))
+
+
+print("\n")
+
+
+print_q3(q3)
